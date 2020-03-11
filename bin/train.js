062c78b5d48d5bbbf886416f752cc7987437d5f2
Fedor Indutny <fedor@indutny.com>
2020-03-10 21:13:18 -0700

    lib: adjustments

--- bin/train.js
+++ bin/train.js
@@ -3,7 +3,9 @@ import yargs from 'yargs';
 import path from 'path';
 
 import { RNNModel } from '../lib/model/index.js';
-import { Maze, Sequence, Bandit, TicTacToe } from '../lib/env/index.js';
+import {
+  Maze, Sequence, Bandit, TicTacToe, Prisoner,
+} from '../lib/env/index.js';
 import Train from '../lib/train.js';
 
 const argv = yargs
