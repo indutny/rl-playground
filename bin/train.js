821ed417f46fc52e1c5871c80bbef186e485db6d
Fedor Indutny <fedor@indutny.com>
2020-03-11 18:27:49 -0700

    train: dynamic states

--- bin/train.js
+++ bin/train.js
@@ -20,7 +20,7 @@ const argv = yargs
   .option('dump-heap-every', { type: 'number', default: 0 })
   .argv;
 
-const t = new Train(RNNModel, Sequence, {
+const t = new Train(RNNModel, Maze, {
   lr: argv.lr,
   batchSize: argv['batch-size'],
   maxSteps: argv['max-steps'],
